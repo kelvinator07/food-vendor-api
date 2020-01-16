99a5e45b3ed400d3faa1bd8dad55ff62b3c58ab6
Kelvinator07 <kelvinator4leo@gmail.com>
2020-01-17 00:59:05 +0100

    Initial commit

---
+++ src/main/java/com/byteworks/ByteworksFoodVendorApiApplication.java
@@ -0,0 +1,13 @@
+package com.byteworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteworksFoodVendorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteworksFoodVendorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byteworks/ByteworksFoodVendorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byteworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteworksFoodVendorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
