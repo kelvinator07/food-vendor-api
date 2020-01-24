fa54209b50ec726380ab6912954e8d9399a320fc
Kelvinator07 <kelvinator4leo@gmail.com>
2020-01-24 21:01:12 +0100

    Web and Security Configurations

---
+++ src/main/java/com/byteworks/configuration/WebConfig.java
@@ -0,0 +1,61 @@
+package com.byteworks.configuration;
+
+import com.byteworks.service.UserSecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserSecurityService userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private static final String[] PUBLIC_MATCHERS = {
+            "/",
+            "/h2-console/**",
+            "/v2/api-docs/**",
+            "/swagger-ui.html/**",
+            "/configuration/**",
+            "/swagger-resources/**",
+            "/webjars/**",
+            "/api/v1/menu",
+            "/api/v1/auth/register",
+            "/api/v1/auth/login"
+    };
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+        .and().headers().frameOptions().disable();
+    }
+
+    @Bean
+    public AuthenticationManager customAuthenticationManager() throws Exception {
+        return authenticationManager();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
+}
