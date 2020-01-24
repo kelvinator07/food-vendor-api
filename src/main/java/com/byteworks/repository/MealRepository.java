fd6f9d08ca1c203d1d12780c87ddabe4a35d0203
Kelvinator07 <kelvinator4leo@gmail.com>
2020-01-24 21:11:29 +0100

    Implemented Jpa Repositories for Entities

---
+++ src/main/java/com/byteworks/repository/MealRepository.java
@@ -0,0 +1,9 @@
+package com.byteworks.repository;
+
+import com.byteworks.model.Meal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MealRepository extends JpaRepository<Meal, Long> {
+}
---
+++ src/main/java/com/byteworks/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.byteworks.repository;
+
+import com.byteworks.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
